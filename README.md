bb80795b5caa2431ce4fa654bed9eaf6912ce14a
Mike Wallio <michael.wallio@outlook.com>
2025-01-15 10:56:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# FZF ZSH Plugin
+
+This is a plugin for zsh heavily inspired by [PatrickF1/fzf.fish](https://github.com/PatrickF1/fzf.fish).
+
+It's still a work in progress, and has slightly different keybindings.
+
+## Keybindings
+
+| Keybinding   | Description                            |
+| ------------ | -------------------------------------- |
+| `Ctrl+T`     | File search using fzf                  |
+| `Ctrl+R`     | History search using fzf               |
+| `Alt+c`      | Change directory using fzf             |
+| `Ctrl+Alt+L` | Git log search using fzf               |
+| `Ctrl+Alt+T` | Git status search using fzf            |
+| `Ctrl+V`     | Environment variables search using fzf |
---
+++ fzf.plugin.zsh
@@ -0,0 +1,122 @@
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# Override for zsh integration
+autoload -Uz compinit && compinit
+autoload -Uz bashcompinit && bashcompinit
+
+export FZF_DEFAULT_OPTS="--ansi"
+export FZF_PREVIEW_CMD="${FZF_PREVIEW_CMD:-bat --style=numbers --color=always --line-range :500}"
+
+fzf_find_files() {
+  if command -v fd > /dev/null 2>&1; then
+    fd --type file --color=always
+  else
+    if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+      find . -type f ! -path '*/.*' -print 2>/dev/null | grep -vFf <(git ls-files --others --ignored --exclude-standard --directory)
+    else
+      find . -type f ! -path '*/.*' -print 2>/dev/null
+    fi
+  fi
+}
+
+# File search using fzf
+fzf-file-widget() {
+  local selected_file
+  local hint=""
+  if [[ "$LBUFFER" =~ [^[:space:]]$ ]]; then
+    hint="${LBUFFER##* }"
+  fi
+  selected_file=$(fzf_find_files | fzf --preview "$FZF_PREVIEW_CMD {}" --height 40% --query="$hint")
+  if [[ -n "$selected_file" ]]; then
+    LBUFFER="${LBUFFER%$hint}$selected_file"
+  fi
+  zle redisplay
+}
+
+zle -N fzf-file-widget
+bindkey '^T' fzf-file-widget
+
+# History search using fzf
+fzf-history-widget() {
+  local selected_command
+  local query="${BUFFER}"
+  selected_command=$(fc -lnr 1 | fzf --height 40% --query="$query")
+  if [[ -n "$selected_command" ]]; then
+    BUFFER="$selected_command"
+    CURSOR=${#BUFFER}  # Move cursor to the end of the buffer
+  fi
+  zle redisplay
+}
+zle -N fzf-history-widget
+bindkey '^R' fzf-history-widget
+bindkey '^[[A' fzf-history-widget
+
+# Directory search using fzf
+fzf-cd-widget() {
+  local selected_dir
+  if command -v fd > /dev/null 2>&1; then
+    selected_dir=$(fd --type d --color=always | fzf --preview '[[ -d {} ]] && (command -v eza > /dev/null 2>&1 && eza -l --color=always {} || ls -l --color=always {})' --height 40%)
+  else
+    selected_dir=$(find . -type d ! -path '*/.*' 2>/dev/null | fzf --preview '[[ -d {} ]] && (command -v eza > /dev/null 2>&1 && eza -l --color=always {} || ls -l --color=always {})' --height 40%)
+  fi
+  if [[ -n "$selected_dir" ]]; then
+    cd "$selected_dir"
+    BUFFER=
+    zle accept-line
+  fi
+  zle reset-prompt
+}
+
+zle -N fzf-cd-widget
+bindkey '^[c' fzf-cd-widget
+
+# Git log search using fzf
+fzf-git-log-widget() {
+  local selected_commit
+  if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    selected_commit=$(git log --oneline --color 2>/dev/null | fzf --preview 'git show --color=always --stat --patch {1}' --height 40% --preview-window=right:70% | awk '{print $1}')
+  else
+    selected_commit=""
+  fi
+  if [[ -n "$selected_commit" ]]; then
+    LBUFFER="${LBUFFER}$selected_commit"
+  fi
+  zle redisplay
+}
+
+zle -N fzf-git-log-widget
+bindkey '^[^L' fzf-git-log-widget  # ctrl+alt+l
+
+# Git status search using fzf
+fzf-git-status-widget() {
+  local selected_file
+  if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    selected_file=$(git status --short | fzf --preview '[[ $(git ls-files --error-unmatch {2} 2>/dev/null) ]] && git diff --color=always {2} || (echo "Untracked:" && bat --style=numbers --color=always --line-range :500 {2})' --height 40% --preview-window=right:70% | awk '{print $2}')
+  else
+    selected_file=""
+  fi
+  if [[ -n "$selected_file" ]]; then
+    LBUFFER="${LBUFFER}$selected_file"
+  fi
+  zle redisplay
+}
+
+zle -N fzf-git-status-widget
+bindkey '^[^T' fzf-git-status-widget  # ctrl+alt+s
+
+fzf-variables-widget() {
+  local selected_variable
+  local hint=""
+  if [[ "$LBUFFER" =~ [^[:space:]]$ ]]; then
+    hint="${LBUFFER##* }"
+  fi
+  selected_variable=$(env | fzf --height 40% --query="$hint" | awk -F'=' '{print $1}')
+  if [[ -n "$selected_variable" ]]; then
+    LBUFFER="${LBUFFER%$hint}\$$selected_variable"
+  fi
+  zle redisplay
+}
+
+zle -N fzf-variables-widget
+bindkey '^V' fzf-variables-widget  # ctrl+shift+f
