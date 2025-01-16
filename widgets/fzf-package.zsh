adc3d45fcf47ef205559a7570369781a7abda563
Mike Wallio <michael.wallio@outlook.com>
2025-01-15 21:57:14 -0500

    Revert "Use fd if available"
    This reverts commit 36426b78752763409d4c2bef5316e9bce59cd216.

--- widgets/fzf-package.zsh
+++ widgets/fzf-package.zsh
@@ -4,11 +4,7 @@ fzf-package-widget() {
   if [[ "$LBUFFER" =~ [^[:space:]]$ ]]; then
     hint="${LBUFFER##* }"
   fi
-  if command -v fd > /dev/null 2>&1; then
-    selected_packages=$(fd -e json -E 'node_modules' -x jq -r '.name' {} | awk '!seen[$0]++' | fzf --height 60% --prompt="Select package(s): " --query="$hint" --multi | tr '\n' ' ')
-  else
-    selected_packages=$(find . -name 'package.json' -not -path '*/node_modules/*' -exec jq -r '.name' {} + | awk '!seen[$0]++' | fzf --height 60% --prompt="Select package(s): " --query="$hint" --multi | tr '\n' ' ')
-  fi
+  selected_packages=$(find . -name 'package.json' -not -path '*/node_modules/*' -exec jq -r '.name' {} + | awk '!seen[$0]++' | fzf --height 60% --prompt="Select package(s): " --query="$hint" --multi | tr '\n' ' ')
   if [[ -n "$selected_packages" ]]; then
     LBUFFER="${LBUFFER%$hint}$selected_packages"
   fi
