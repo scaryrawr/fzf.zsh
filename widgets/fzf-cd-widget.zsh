0ae02301a0575e88fc4d6df025e1f1b3289ed927
Mike Wallio <michael.wallio@outlook.com>
2025-01-17 15:48:48 -0500

    Add support for `fd` not being installed

--- widgets/fzf-cd-widget.zsh
+++ widgets/fzf-cd-widget.zsh
@@ -3,7 +3,11 @@ fzf-cd-widget() {
   local selected_dir
   local hint="${LBUFFER}"
   BUFFER=
-  selected_dir=$(fd --type d --color=always | fzf --preview "$FZF_PREVIEW_CMD {}" --height 60% --query="$hint")
+  if command -v fd > /dev/null 2>&1; then
+    selected_dir=$(fd --type d --color=always | fzf --preview "$FZF_PREVIEW_CMD {}" --height 60% --query="$hint")
+  else
+    selected_dir=$(find . -type d -not -path '*/\.*' -not -path '.' | sed 's|^\./||' | fzf --preview "$FZF_PREVIEW_CMD {}" --height 60% --query="$hint")
+  fi
   if [[ -n "$selected_dir" ]]; then
     cd "$selected_dir"
     zle accept-line
