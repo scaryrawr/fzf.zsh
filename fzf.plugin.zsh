903cbdb03e36155080f231b9db25fbfa6a16de1d
Mike Wallio <michael.wallio@outlook.com>
2025-01-17 22:02:36 -0500

    Move preview scripts to previewers directory

--- fzf.plugin.zsh
+++ fzf.plugin.zsh
@@ -8,11 +8,11 @@ autoload -Uz compinit && compinit
 autoload -Uz bashcompinit && bashcompinit
 
 export FZF_DEFAULT_OPTS="--ansi"
-export FZF_PREVIEW_CMD="${FZF_PREVIEW_CMD:-$SCRIPT_DIR/fzf_preview}"
-export FZF_GIT_STATUS_PREVIEW_CMD="${FZF_GIT_STATUS_PREVIEW_CMD:-$SCRIPT_DIR/fzf_git_status_preview}"
-export FZF_GIT_BLAME_PREVIEW_CMD="${FZF_GIT_BLAME_PREVIEW_CMD:-$SCRIPT_DIR/fzf_git_blame_preview}"
-export FZF_GIT_COMMIT_PREVIEW_CMD="${FZF_GIT_COMMIT_PREVIEW_CMD:-$SCRIPT_DIR/fzf_git_commit_preview}"
-export FZF_PACKAGE_PREVIEW_CMD="${FZF_PACKAGE_PREVIEW_CMD:-$SCRIPT_DIR/fzf_package_preview}"
+export FZF_PREVIEW_CMD="${FZF_PREVIEW_CMD:-$SCRIPT_DIR/previewers/fzf_preview}"
+export FZF_GIT_STATUS_PREVIEW_CMD="${FZF_GIT_STATUS_PREVIEW_CMD:-$SCRIPT_DIR/previewers/fzf_git_status_preview}"
+export FZF_GIT_BLAME_PREVIEW_CMD="${FZF_GIT_BLAME_PREVIEW_CMD:-$SCRIPT_DIR/previewers/fzf_git_blame_preview}"
+export FZF_GIT_COMMIT_PREVIEW_CMD="${FZF_GIT_COMMIT_PREVIEW_CMD:-$SCRIPT_DIR/previewers/fzf_git_commit_preview}"
+export FZF_PACKAGE_PREVIEW_CMD="${FZF_PACKAGE_PREVIEW_CMD:-$SCRIPT_DIR/previewers/fzf_package_preview}"
 
 # Lazy load widget functions
 lazy_load_widget() {
