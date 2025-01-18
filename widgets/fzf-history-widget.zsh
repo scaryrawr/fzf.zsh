435c2280602644da74c2111a4cc6818ee6676288
Mike Wallio <michael.wallio@outlook.com>
2025-01-17 21:30:10 -0500

    Update history to look more similar to fzf.fish

--- widgets/fzf-history-widget.zsh
+++ widgets/fzf-history-widget.zsh
@@ -2,7 +2,7 @@
 fzf-history-widget() {
   local selected_command
   local query="${BUFFER}"
-  selected_command=$(fc -lnr 1 | awk '!seen[$0]++' | fzf --height 60% --query="$query")
+  selected_command=$(fc -flnr 1 | awk '!seen[$0]++' | fzf --height 60% --preview="awk '{\$1=\"\"; \$2=\"\"; sub(/^ */, \"\"); print \$0}' <<< {}" --preview-window bottom:3:wrap --query="$query" | awk '{$1=""; $2=""; sub(/^ */, ""); print $0}')
   if [[ -n "$selected_command" ]]; then
     BUFFER="$selected_command"
     CURSOR=${#BUFFER}  # Move cursor to the end of the buffer
