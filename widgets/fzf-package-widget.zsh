5314bb8d6b07b039531d3478eeb0be022902a484
Mike Wallio <michael.wallio@outlook.com>
2025-02-19 15:46:59 -0500

    Switch to using workspace info

--- widgets/fzf-package-widget.zsh
+++ widgets/fzf-package-widget.zsh
@@ -4,11 +4,13 @@ fzf-package-widget() {
   if [[ "$LBUFFER" =~ [^[:space:]]$ ]]; then
     hint="${LBUFFER##* }"
   fi
-  if command -v fd > /dev/null 2>&1; then
-    selected_packages=$(fd -t f 'package\.json' -x jq -r '.name' {} | awk '!seen[$0]++' | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {}" --query="$hint" --multi | tr '\n' ' ')
-  else
-    selected_packages=$(find . -name 'package.json' -not -path '*/node_modules/*' -exec jq -r '.name' {} + | awk '!seen[$0]++' | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {}" --query="$hint" --multi | tr '\n' ' ')
+
+  if [[ ! -f "package.json" ]]; then
+    return 1
   fi
+  
+  selected_packages=$(yarn --json workspaces info | jq -r '.data' | jq -r 'keys | .[]' | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {}" --query="$hint" --multi | tr '\n' ' ')
+
   if [[ -n "$selected_packages" ]]; then
     LBUFFER="${LBUFFER%$hint}$selected_packages"
   fi
