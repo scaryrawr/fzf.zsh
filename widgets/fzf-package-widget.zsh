4b73e266b11da575c5152e6e9a60b55951ea6cab
Mike Wallio <michael.wallio@outlook.com>
2025-02-21 09:18:53 -0500

    Add a package name cache

--- widgets/fzf-package-widget.zsh
+++ widgets/fzf-package-widget.zsh
@@ -9,7 +9,27 @@ fzf-package-widget() {
     return 1
   fi
   
-  selected_packages=$(yarn --json workspaces info | jq -r '.data' | jq -r 'keys[]' | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {}" --query="$hint" --multi | tr '\n' ' ')
+  # Begin caching logic similar to _monorepo_search_workspace.fish
+  local safe_dir cache_dir mod_time cache_file packages_info
+  safe_dir=${PWD//\//_}
+  cache_dir="/tmp/fzf.zsh/$safe_dir"
+  mkdir -p "$cache_dir"
+  if [[ "$(uname)" == "Linux" ]]; then
+    mod_time=$(stat --format=%Y .)
+  else
+    mod_time=$(stat -f %m .)
+  fi
+  cache_file="$cache_dir/$mod_time.json"
+  
+  if [[ -f "$cache_file" ]]; then
+    packages_info=$(<"$cache_file")
+  else
+    packages_info=$(yarn --json workspaces info | jq -r '.data' | jq -r 'keys[]')
+    echo "$packages_info" > "$cache_file"
+  fi
+  # End caching logic
+  
+  selected_packages=$(echo "$packages_info" | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {}" --query="$hint" --multi | tr '\n' ' ')
 
   if [[ -n "$selected_packages" ]]; then
     LBUFFER="${LBUFFER%$hint}$selected_packages"
