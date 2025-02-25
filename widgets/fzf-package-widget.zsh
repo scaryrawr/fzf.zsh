f29bd0758d15a833c171e9b8192a282c6068e18a
Mike Wallio <michael.wallio@outlook.com>
2025-02-25 16:42:06 -0500

    Add cargo support

--- widgets/fzf-package-widget.zsh
+++ widgets/fzf-package-widget.zsh
@@ -1,16 +1,26 @@
 fzf-package-widget() {
-  local hint=""
+  local hint=''
   if [[ "$LBUFFER" =~ [^[:space:]]$ ]]; then
     hint="${LBUFFER##* }"
   fi
 
-  if [[ ! -f "package.json" ]]; then
-    return 1
-  fi
-  
   local safe_dir=${PWD//\//_}
   local cache_dir="/tmp/fzf.zsh/$safe_dir"
-  local hash=$(git ls-files '*package.json' | xargs sha256sum | sha256sum | awk '{print $1}')
+
+  local hash=''
+  if [[ -f 'package.json' ]]; then
+    hash=$(git ls-files '*package.json' | xargs sha256sum | sha256sum | awk '{print $1}')
+  fi
+
+  if [[ -f 'Cargo.toml' ]]; then
+    local cargo_hash=$(git ls-files '*Cargo.toml' | xargs sha256sum | sha256sum | awk '{print $1}')
+    local combine="${hash}-${cargo_hash}"
+    hash=$(echo "$combine" | sha256sum | awk '{print $1}')
+  fi
+
+  if [[ -z "$hash" ]]; then
+    return 1
+  fi
 
   local cache_file="$cache_dir/$hash.json"
   local packages_info
@@ -18,10 +28,20 @@ fzf-package-widget() {
     packages_info=$(<"$cache_file")
   else
     mkdir -p "$cache_dir"
-    packages_info=$(yarn --json workspaces info | jq -r '.data' | jq -r 'to_entries | map({name: .key, path: ("./" + .value.location + "/package.json")})' | tee "$cache_file")
+    echo '[]' > "$cache_file"
+    if [[ -f 'package.json' ]]; then
+      yarn --json workspaces info | jq -r '.data' | jq -r 'to_entries | map({name: .key, path: ("./" + .value.location + "/package.json")})' > "$cache_file"
+    fi
+    
+    if [[ -f 'Cargo.toml' ]]; then
+      cargo metadata --format-version 1 | jq -r '.packages | map(select(.id | startswith("path+file")) | {name: .name, path: .manifest_path})' | jq -s '.[0] + .[1]' "$cache_file" - > "$cache_file.tmp"
+      mv "$cache_file.tmp" "$cache_file"
+    fi
+    
+    packages_info=$(<"$cache_file")
   fi
   
-  local selected_packages=$(echo "$packages_info" | jq -r '.[].name' | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {} '$cache_file'" --query="$hint" --multi | tr '\n' ' ')
+  local selected_packages=$(echo "$packages_info" | jq -r '.[].name' | awk '!seen[$0]++' | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {} '$cache_file'" --query="$hint" --multi | tr '\n' ' ')
 
   if [[ -n "$selected_packages" ]]; then
     LBUFFER="${LBUFFER%$hint}$selected_packages"
