e7bcb2dcf5521ac98f11084ed6971f9a0e4759f4
Mike Wallio <michael.wallio@outlook.com>
2025-10-08 09:19:11 -0400

    Remove variable contents from list (#3)
    Uses a preview to show the value

--- widgets/fzf-variables-widget.zsh
+++ widgets/fzf-variables-widget.zsh
@@ -3,7 +3,7 @@ fzf-variables-widget() {
 	if [[ "$LBUFFER" =~ [^[:space:]]$ ]]; then
 		hint="${LBUFFER##* }"
 	fi
-	local selected_variable=$(env | fzf --height 60% --query="$hint" | awk -F'=' '{print $1}')
+	local selected_variable=$(env | sort | awk -F'=' '{print $1}' | fzf --height 60% --preview 'env | awk -F= "/^{r}=/ {print \$2}"' --query="$hint" )
 	if [[ -n "$selected_variable" ]]; then
 		LBUFFER="${LBUFFER%$hint}\$$selected_variable"
 	fi
