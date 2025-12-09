73162a155c24d3e9a65bc2ea1a31c1edb33afbba
Mike Wallio <michael.wallio@outlook.com>
2025-12-09 10:37:41 -0500

    Use subshell to remove blocking

--- widgets/fzf-package-widget.zsh
+++ widgets/fzf-package-widget.zsh
@@ -4,14 +4,11 @@ fzf-package-widget() {
 		hint="${LBUFFER##* }"
 	fi
 
-	local packages=$(rg --files --glob 'package.json' | xargs jq -r '.name + "\t" + input_filename')
-	local crates=$(cargo metadata --format-version 1 2>/dev/null | jq -r '.packages | map(select(.id | startswith("path+file")) | .name + "\t" + .manifest_path) | .[]')
-
-	packages=${packages:-''}
-	crates=${crates:-''}
-	local packages_info=$(printf '%s\n%s' "$packages" "$crates")
-
-	local selected_packages=$(echo "$packages_info" | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {1} {2}" --query="$hint" --multi --delimiter=$'\t' --with-nth=1 --bind="ctrl-o:execute(${EDITOR:-vim} {2})" | cut -d$'\t' -f1 | tr '\n' ' ')
+	local selected_packages=$({
+		rg --files --glob 'package.json' | xargs -r jq -r '.name + "\t" + input_filename' 2>/dev/null &
+		cargo metadata --format-version 1 2>/dev/null | jq -r '.packages | map(select(.id | startswith("path+file")) | .name + "\t" + .manifest_path) | .[]' &
+		wait
+	} | fzf --height 60% --prompt="Select package(s): " --preview "$FZF_PACKAGE_PREVIEW_CMD {1} {2}" --query="$hint" --multi --delimiter=$'\t' --with-nth=1 --bind="ctrl-o:execute(${EDITOR:-vim} {2})" | cut -d$'\t' -f1 | tr '\n' ' ')
 
 	if [[ -n "$selected_packages" ]]; then
 		LBUFFER="${LBUFFER%$hint}$selected_packages"
