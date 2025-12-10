1a715155e40c06267c80ba1e602fd9c05a23ec1e
Mike Wallio <michael.wallio@outlook.com>
2025-12-09 22:19:19 -0500

    Add FZF_FD_OPTS support for file and directory search

--- widgets/fzf-cd-widget.zsh
+++ widgets/fzf-cd-widget.zsh
@@ -2,7 +2,7 @@
 fzf-cd-widget() {
 	local selected_dir
 	local hint="${LBUFFER}"
-	selected_dir=$(fd --type d --color=always | fzf --preview "$FZF_PREVIEW_CMD {}" --height 60% --query="$hint")
+	selected_dir=$(fd $FZF_FD_OPTS --type d --color=always | fzf --preview "$FZF_PREVIEW_CMD {}" --height 60% --query="$hint")
 	if [[ -n "$selected_dir" ]]; then
 		cd "$selected_dir"
 		zle accept-line
